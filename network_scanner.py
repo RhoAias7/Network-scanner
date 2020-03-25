7c90983c86dd77ab4b9c75d027d14d97633576f1
root <ayofouad@gmail.com>
2020-03-25 09:50:54 +0000

    Completed network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python
+import argparse
 
 import scapy.all as scapy
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", dest="ip", help="New ip range")
+    options = parser.parse_args()
+    if not options.ip:
+        parser.error("[-] Please specify an ip range, use --help for more info")
+    return options
+
+
+options = get_args()
+
+
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for response in answered_list:
+        client_dictionary = {"ip": response[1].psrc, "mac": response[1].hwsrc}
+        clients_list.append(client_dictionary)
+    return clients_list
 
 
-scan("10.0.2.1/24")
+def print_result(result_list):
+    print("IP\t\t\tMAC Address\n-----------------------------------------------------")
+    for client in result_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+
+print_result(scan(options.ip))
